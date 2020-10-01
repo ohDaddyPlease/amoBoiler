78dd0667e83e32d481f949834614f6e5bce1066f
Serj <kraspvp@mail.ru>
2020-10-02 00:58:44 +0700

    ,

--- index.php
+++ index.php
@@ -2,14 +2,19 @@
 
 use AmoCRM\Exceptions\AmoCRMApiException;
 use AmoCRM\Models\ContactModel;
+use AmoCRM\Models\LeadModel;
 
 require_once 'configure.php';
 
 $contact = new ContactModel();
 $contact->setName('Контакт, созданный через OAuth2.0');
 
+$lead = new LeadModel();
+$lead->setPrice(123)->setName('Сделка, созданная через OAuth2.0');
+
 try {
     $contactModel = $apiClient->contacts()->addOne($contact);
+    $leadModel = $apiClient->leads()->addOne($lead);
 } catch (AmoCRMApiException $e) {
     var_dump($e);
 }
